a405af2fedf8cba471e11e49287c8976703b58aa
stefan_flo <stefan@flothemes.com>
2023-02-18 23:21:57 +0200

    print members

--- src/components/Area/Area.jsx
+++ src/components/Area/Area.jsx
@@ -1,11 +1,16 @@
+import Group from "../Group/Group";
 import "./Area.scss";
 
-function Area({ name }) {
+function Area({ name, members }) {
   return (
     <div className="area">
       <h4 className="area__name">{name}</h4>
 
-      <div className="area__groups"></div>
+      <div className="area__groups">
+        {members.map((member, index) => {
+          return <Group member={member} key={index} />;
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Group/Group.jsx
+++ src/components/Group/Group.jsx
@@ -1,7 +1,21 @@
+import Member from "../Member/Member";
 import "./Group.scss";
 
-function Group() {
-  return <div className="group"></div>;
+function Group({ member }) {
+  return (
+    <div className="group">
+      {member.map((group, index) => {
+        return (
+          <Member
+            key={group.id}
+            sku={group.sku}
+            defaultSku={group.defaultSku}
+            status={index === 0 ? group.status : null}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default Group;
