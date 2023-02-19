503b129ab7d6b9218a7176c1b309d080b22d4805
stefan_flo <stefan@flothemes.com>
2023-02-19 14:57:43 +0200

    lint configurations

--- src/components/Member/Member.jsx
+++ src/components/Member/Member.jsx
@@ -4,9 +4,7 @@ import "./Member.scss";
 function Member({ sku, defaultSku, status }) {
   return (
     <div className="member">
-      {status && (
-        <div className={`member__status member__status--${status}`}></div>
-      )}
+      {status && <div className={`member__status member__status--${status}`}></div>}
       <h3 className="member__sku">{sku}</h3>
       <h5 className="member__default-sku">{defaultSku}</h5>
     </div>
